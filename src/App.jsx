967cce4c8f3de4986aa926db4fa123c73db9db06
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-01-13 17:48:11 -0500

    added more information to learning about cluster

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AboutUs from "./AboutUs";
 import LogoWithoutText from "./static/logo_without_text.png";
 import Navbar from "./nav-bar";
 import KafkaCompassPerformanceStatsDemo from "./static/KafkaCompassPerformanceStatsDemo.gif";
+import contentMonitoringScreenshot from "./static/contentMonitoringScreenshot.png"
 
 const App = (props) => {
     const myRef = useRef(null);
@@ -19,7 +20,7 @@ const App = (props) => {
         <main>
             <Navbar />
             <div className="landing-container h-screen">
-            <div className="flex-1 justify-around">
+            <div className="flex-1 justify-around ">
             <article className="font-mono">
                 <img className="icon-logo my-2" src={LogoWithoutText} />
                 <h2 className="page-title text-lg ">Kafka Compass</h2>
@@ -37,18 +38,18 @@ const App = (props) => {
                 <div className="flex justify-around">
                 <div className="landing-buttons my-3 flex">
                         <form action="https://github.com/oslabs-beta/KafkaCompass">
-                        <button className="btn btn-outline"> Navigate to Github Repository </button>
+                        <button className="btn btn-outline"> Github Repository </button>
                         </form>
-                        <button onClick={executeScroll} className="btn btn-outline" >Meet the team</button>
+                        <button onClick={executeScrollToInfo} className="btn btn-outline" >Learn More</button>
                      </div>
                 </div>
             <article><h2 className="page-title text-lg"><i>Don't get lost navigating your Kafka clusters - grab a compass!</i></h2></article>
-            <div className="flex justify-around"><button onClick={executeScrollToInfo} className="btn btn-outline" >Learn More</button></div>
+            <div className="flex justify-around"><button onClick={executeScroll} className="btn btn-outline" >Meet the team</button></div>
             </div>
             </div>
             <div>
             <article ref={infoRef} className="flex-1 flex-row justify-around pb-10 px-10">
-                <div><img className="rounded" src={KafkaCompassPerformanceStatsDemo}></img></div>
+                <div><img className="rounded-lg" src={KafkaCompassPerformanceStatsDemo}></img></div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <div className="card-body">
                         <h2 className="card-title">View an advanced suite of cluster metrics in Performance Statistics</h2>
@@ -67,7 +68,7 @@ const App = (props) => {
                         </div>
                     </div>
                 </div>
-                <div><img className="rounded" src={KafkaCompassPerformanceStatsDemo}></img></div>
+                <div><img className="rounded-lg" src={contentMonitoringScreenshot} height='1000px' width='600p' ></img></div>
             </ article>
             </div>
             <div ref={myRef}>
